7a15f0c47ebb6a6f2ee6a7d22d7d57ebed133361
Sawe-Naomy <naomyjsaw@gmail.com>
2024-09-17 14:16:05 +0300

    updated JS file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: "Roses are red pick me one",
     autoStart: true,
     delay: 1,
     cursor: "",
